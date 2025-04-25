ffd92d1b7c826e0583f5c5e5937ab8228015d6f6
Syed Safdar Ali Shah <programmersafdar@live.com>
2025-04-25 06:40:11 +0500

    docker working good

--- src/fastapi_docker/__init__.py
+++ src/fastapi_docker/__init__.py
@@ -3,9 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    main()
-    return {"/": "Hello from fastapi-docker!"}
-
-def main() -> str:
-    return "Hello from fastapi-docker!"
+async def get_root():
+    return {"message": "Hello, World!"}
